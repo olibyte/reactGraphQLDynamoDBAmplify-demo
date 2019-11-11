343e374b5198702ddd4465d7b92e55464b3b0f07
Oliver <ocben1@gmail.com>
2019-11-11 17:28:50 +1100

    add user auth to a react app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Auth from 'aws-amplify';
+import awsConfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+Auth.configure(awsConfig);
+
 
 function App() {
   return (
@@ -23,4 +28,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
